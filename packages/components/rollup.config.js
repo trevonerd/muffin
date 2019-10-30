9c85d5825fde3573ff76436869362bcc847a56c1
Marco Trevisani <marco.trevisani@ynap.com>
2019-10-30 10:45:37 +0100

    new components bundle

--- packages/components/rollup.config.js
+++ packages/components/rollup.config.js
@@ -1,15 +1,30 @@
 import multiEntry from 'rollup-plugin-multi-entry';
 import babel from 'rollup-plugin-babel';
-// import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
 
 module.exports = [
     {
         input: 'src/components/**/*.js',
         output: {
             file: 'dist/components.js',
-            format: 'cjs'
+            format: 'umd',
+            name: 'muffin'
         },
-        plugins: [multiEntry(), babel()],
-        external: ['react', '@material-ui/core/Button'] // <-- suppresses the warning]
+        plugins: [
+            multiEntry(),
+            resolve(),
+            babel({
+                exclude: 'node_modules/**'
+            }),
+            commonjs({
+                include: /node_modules/,
+                namedExports: {
+                    'react-is': ['ForwardRef'],
+                    'prop-types': ['elementType']
+                }
+            })
+        ],
+        external: ['react', 'react-dom']
     }
 ];
